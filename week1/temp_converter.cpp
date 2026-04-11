6d60fa54044ec48aac9ed724789c818b07eec491
SquidTSG <62303411+SquidTSG@users.noreply.github.com>
2026-04-11 22:45:57 +0000

    5

---
+++ week1/temp_converter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double temperature;
+    char unit;
+    double convertedTemp;
+
+    std::cout << "Temp Converter" << std::endl;
+
+    std::cout << "enter temperature Value ";
+    std::cin >> temperature;
+
+    std::cout << "enter unit (C for Celsius, F for Fahrenheit): ";
+    std::cin >> unit;
+
+    if (unit == 'C' || unit == 'c') {
+        convertedTemp = (temperature * 9.0 / 5.0) + 32;
+        std::cout <<  temperature << "°C is equal to " << convertedTemp << "°F" << std::endl;
+    }
+    else if (unit == 'F' || unit == 'f') {
+        convertedTemp = (temperature -32) * 5.0 / 9.0;
+        std::cout <<  temperature << "°F is equal to " << convertedTemp << "°C" << std::endl;
+    }
+    else {
+        std::cout << "invalid unit entered." << std::endl;
+    }
+
+    return 0;
+}
