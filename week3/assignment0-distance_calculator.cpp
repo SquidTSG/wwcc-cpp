6d88d351a4e57611612bb9db8e3177171c323e3c
SquidTSG <62303411+SquidTSG@users.noreply.github.com>
2026-04-23 06:40:15 +0000

    30

---
+++ week3/assignment0-distance_calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double x1, y1, x2, y2;
+    
+    cout << "Enter the coordinates of the first point:" << endl;
+    cout << "x1: ";
+    cin >> x1;
+    cout << "y1: ";
+    cin >> y1;
+    
+    cout << "Enter the coordinates of the second point:" << endl;
+    cout << "x2: ";
+    cin >> x2;
+    cout << "y2: ";
+    cin >> y2;
+    
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    
+    cout << fixed << setprecision(2);
+    cout << "The distance between (" << x1 << "," << y1 << ") and (" << x2 << "," << y2 << ") is: " << distance << endl;
+    
+    return 0;
+}
